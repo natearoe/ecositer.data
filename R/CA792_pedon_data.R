202036e4cfc638b59e665da4a37d6448afb2b3d8
Roe <12001003012275@fedidcard.gov>
2025-01-06 11:45:04 -0800

    Initial commit of ecositer.data package

---
+++ R/CA792_pedon_data.R
@@ -0,0 +1,14 @@
+#' Example Dataset: CA792_pedon_data
+#'
+#' A sample dataset containing pedon data from the CA792 Soil Survey - Sequoia and Kings Canyon National Parks.
+#'
+#' @format A SoilProfileCollection
+#' @source CA792 Soil Survey
+#' @usage data(CA792_pedon_data)
+#' @examples
+#' # Load the dataset
+#' data(CA792_pedon_data)
+#'
+#' # Display the first few rows
+#' head(big_data)
+"CA792_pedon_data"
