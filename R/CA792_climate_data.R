61a0494be2e7dbcf9cad1d7b8f9f98af8d607cbe
Roe <12001003012275@fedidcard.gov>
2025-01-06 21:04:44 -0800

    add

--- R/CA792_climate_data.R
+++ R/CA792_climate_data.R
@@ -1,9 +1,9 @@
 #' Example Dataset: CA792_climate_data
 #'
-#' A sample dataset containing climate data from the CA792 Soil Survey (Sequoia and Kings Canyon National Parks),
-#' calculated using ecositerSpatial::site_prism_annual_normals()
+#' A sample dataset containing climate data from the CA792 Soil Survey - Sequoia and Kings Canyon National Parks, created using
+#' ecositerSpatial::site_prism_annual_normals()
 #'
-#' @format dataframe
+#' @format A SoilProfileCollection
 #' @source CA792 Soil Survey
 #' @usage data(CA792_climate_data)
 #' @examples
