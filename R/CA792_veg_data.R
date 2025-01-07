d1c8eeb868fa13278a0fa1946ab150a011655280
Roe <12001003012275@fedidcard.gov>
2025-01-06 18:56:56 -0800

    create CA792_veg_data

---
+++ R/CA792_veg_data.R
@@ -0,0 +1,14 @@
+#' Example Dataset: CA792_veg_data
+#'
+#' A sample dataset containing veg data from the CA792 Soil Survey - Sequoia and Kings Canyon National Parks.
+#'
+#' @format dataframe
+#' @source CA792 Soil Survey
+#' @usage data(CA792_veg_data)
+#' @examples
+#' # Load the dataset
+#' data(CA792_veg_data)
+#'
+#' # Display the first few rows
+#' head(CA792_veg_data)
+"CA792_veg_data"
